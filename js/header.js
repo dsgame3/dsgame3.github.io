ba7b5b96633b373ca0ecd5e2c8e3b7a9d45cc34b
dylansantwani <144082336+dylansantwani@users.noreply.github.com>
2025-02-07 19:07:55 -0600

    add

--- js/header.js
+++ js/header.js
@@ -2,7 +2,7 @@ document.write(`
   <header>
     <a class="title" href="/">
       <img src="/images/logo/opium-clear.png" width="128" height="128">
-      <h2>DSGAME4</h2>
+      <h2>DSGAME</h2>
     </a>
     <nav>
       <a href="/games.html"><i class="fas fa-gamepad"></i></a>
@@ -12,23 +12,3 @@ document.write(`
     </nav>
   </header>
 `);
-
-// lunaris flashback LOL (its an easter egg) >:)
-document.addEventListener('lunaris', function() {
-  const h2Element = document.querySelector('header .title h2');
-  if (h2Element) {
-    h2Element.textContent = 'Lunaris';
-  }
-
-  const imgElement = document.querySelector('header .title img');
-  if (imgElement) {
-    imgElement.src = '/images/logo/lunarisicon.jpeg';
-  }
-
-  const headerElement = document.querySelector('header');
-  if (headerElement) {
-    headerElement.style.backgroundColor = '#2E1FB7';
-  }
-  
-  document.title = 'Game Vault - Lunaris';
-});
